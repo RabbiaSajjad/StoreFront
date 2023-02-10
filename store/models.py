cc7e65c0f7e15a8d3e2546221f5e65c80856a1be
Dev <dev@Devs-MacBook-Pro.local>
2023-02-10 19:54:44 +0500

    Created Models and Migrations

---
+++ store/models.py
@@ -0,0 +1,98 @@
+import email
+from itertools import product
+from tkinter import CASCADE
+from turtle import title
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+
+class Promotion(models.Model):
+  description=models.CharField(max_length=255)
+  discount=models.FloatField()
+
+
+
+class Collection(models.Model):
+  title=models.CharField(max_length=255)
+
+
+class Product(models.Model):
+  title = models.CharField(max_length=255)
+  slug = models.SlugField()
+  description = models.TextField()
+  price = models.DecimalField(max_digits=6, decimal_places=2)
+  inventory = models.IntegerField()
+  sku = models.CharField(max_length=10, primary_key=True)
+  last_updated = models.DateTimeField(auto_now=True)
+  collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+  promotions = models.ManyToManyField(Promotion)
+
+
+class Customer(models.Model):
+  MEMBERSHIP_BRONZE='B'
+  MEMBERSHIP_SILVER='S'
+  MEMBERSHIP_GOLD='G'
+
+  MEMBERSHIP_CHOICES=[
+    (MEMBERSHIP_BRONZE, 'Bronze'),
+    (MEMBERSHIP_SILVER, 'Silver'),
+    (MEMBERSHIP_GOLD, 'Gold'),
+  ]
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  email = models.EmailField(unique=True)
+  phone = models.CharField(max_length=255)
+  birth_date = models.DateField(null=True)
+  membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+  class Meta:
+    db_table='store_customers'
+    indexes=[
+      models.Index(fields=['last_name', 'first_name'])
+    ]
+
+class Address(models.Model):
+  street=models.CharField(max_length=255)
+  city=models.CharField(max_length=255)
+  customer=models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+  zip_code=models.CharField(max_length=255)
+  #in case of one to many relationship between customers and addresses
+  #customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+  PAYMENT_STATUS_PENDING='P'
+  PAYMENT_STATUS_COMPLETE='C'
+  PAYMENT_STATUS_FAILED='F'
+
+  PAYMENT_STATUS_CHOICES=[
+    (PAYMENT_STATUS_PENDING, 'P'),
+    (PAYMENT_STATUS_COMPLETE, 'C'),
+    (PAYMENT_STATUS_FAILED, 'F')
+  ]
+  placed_at=models.DateTimeField(auto_now_add=True)
+  payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+  customer=models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+  sku = models.CharField(max_length=255)
+  quantity= models.PositiveSmallIntegerField()
+  unit_price=models.DecimalField(max_digits=6, decimal_places=2)
+  order=models.ForeignKey(Order, on_delete=models.PROTECT)
+
+class Cart(models.Model):
+  created_at=models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity=models.PositiveSmallIntegerField()
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+
+class Tag(models.Model):
+  label=models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+  tag=models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
